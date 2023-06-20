21398dd9845f56bd74473cad265e62c255d0732e
Davinderpal Singh <78408207+DavinderSingh810@users.noreply.github.com>
2023-06-21 03:48:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
-import path from "path";
+
 
 
 const __filename = fileURLToPath(import.meta.url);
