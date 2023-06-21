954391b10c2c97cba6d0f1318f6022840e85418b
Davinderpal Singh <78408207+DavinderSingh810@users.noreply.github.com>
2023-06-21 23:49:24 +0530

    Update index.jsx

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -15,7 +15,7 @@ const ProfilePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
   const getUser = async () => {
-    const response = await fetch(`https://sociopedia-com.onrender.com/users/${userId}`, {
+    const response = await fetch(`https://sociopedia-com.onrender/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
