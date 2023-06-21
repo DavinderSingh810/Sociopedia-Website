5bbad520ef4cf810a303c50aed9bac8f61e086d3
Davinderpal Singh <78408207+DavinderSingh810@users.noreply.github.com>
2023-06-21 19:58:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,11 +31,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: false }));
 
-app.use(cors({
-  origin: "https://snazzy-dieffenbachia-eac30e.netlify.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "/public/assets")));
 
 /* FILE STORAGE */
