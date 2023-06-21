06298e8adf37d64ed22b7ef78db2a304a2e84420
Davinderpal Singh <78408207+DavinderSingh810@users.noreply.github.com>
2023-06-21 16:11:05 +0530

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -6,6 +6,11 @@ export const createPost = async (req, res) => {
   try {
     const { userId, description, picturePath } = req.body;
     const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
     const newPost = new Post({
       userId,
       firstName: user.firstName,
@@ -17,32 +22,38 @@ export const createPost = async (req, res) => {
       likes: {},
       comments: [],
     });
+
     await newPost.save();
 
-    const post = await Post.find();
-    res.status(201).json(post);
+    const posts = await Post.find();
+    res.status(201).json(posts);
   } catch (err) {
-    res.status(409).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
 /* READ */
 export const getFeedPosts = async (req, res) => {
   try {
-    const post = await Post.find();
-    res.status(200).json(post);
+    const posts = await Post.find();
+    res.status(200).json(posts);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
 export const getUserPosts = async (req, res) => {
   try {
     const { userId } = req.params;
-    const post = await Post.find({ userId });
-    res.status(200).json(post);
+    const posts = await Post.find({ userId });
+
+    if (posts.length === 0) {
+      return res.status(404).json({ message: "User posts not found." });
+    }
+
+    res.status(200).json(posts);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
@@ -51,7 +62,13 @@ export const likePost = async (req, res) => {
   try {
     const { id } = req.params;
     const { userId } = req.body;
+
     const post = await Post.findById(id);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found." });
+    }
+
     const isLiked = post.likes.get(userId);
 
     if (isLiked) {
@@ -68,6 +85,6 @@ export const likePost = async (req, res) => {
 
     res.status(200).json(updatedPost);
   } catch (err) {
-    res.status(404).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 };
